731e106860738f1966cbe500a3f3b94f52fa4107
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-01-02 22:59:23 +0000

    feat: string_len

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include <locale.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
+
+#define failure(...)                                                           \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    exit(1)
 
 enum Color { RED, GREEN };
 
@@ -13,13 +20,18 @@ struct Terminal get_terminal();
 void set_term_fg(struct Terminal *terminal, enum Color color);
 void set_term_bg(struct Terminal *terminal, enum Color color);
 void term_reset(struct Terminal *terminal);
+uint32_t string_len(const char *text);
 
 int main() {
     struct Terminal terminal = get_terminal();
 
-    set_term_fg(&terminal, GREEN);
-    puts("‚±‚ñ‚É‚¿‚Í");
-    term_reset(&terminal);
+    if (setlocale(LC_CTYPE, "") == NULL) {
+        failure("failed to set locale");
+    }
+
+    const char *text = "‚ ‚¢‚¤‚¦‚¨";
+    puts(text);
+    printf("len: %u\n", string_len(text));
 
     term_reset(&terminal);
     return 0;
@@ -68,4 +80,22 @@ void set_term_bg(struct Terminal *terminal, enum Color color) {
 void term_reset(struct Terminal *terminal) {
     SetConsoleTextAttribute(terminal->console_handle,
                             terminal->origin_info.wAttributes);
+}
+
+uint32_t string_len(const char *text) {
+    int32_t count = 0;
+    size_t index = 0;
+
+    while (text[index] != '\0') {
+        int result = mblen(&text[index], MB_CUR_MAX);
+
+        if (result < 0) {
+            failure("mblen returned error: %d", result);
+        }
+
+        index += result;
+        count += 1;
+    }
+
+    return count;
 }
