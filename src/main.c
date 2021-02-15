5a3f936180dda95f91e04645667c27dda46544a4
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-02-16 04:04:34 +0900

    feat: game works!

--- src/main.c
+++ src/main.c
@@ -27,12 +27,13 @@ struct Context {
 };
 
 void render(struct Context *context);
+void on_key_type(struct Context *context, char input);
 
 int main(void) {
     srand((unsigned)time(NULL));
     if (setlocale(LC_CTYPE, "") == NULL) {
         // cannot use failure macro because TERMINAL is not initialized.
-        printf("failed to set locale");
+        fprintf(stderr, "failed to set locale");
         return 1;
     }
 
@@ -48,6 +49,8 @@ int main(void) {
         },
     };
 
+    term_set_cursor_visible(&TERMINAL, false);
+
     do {
         render(&context);
         const struct TerminalEvent event = term_poll_event(&TERMINAL);
@@ -59,6 +62,8 @@ int main(void) {
             if (keycode == ESC_KEY) {
                 context.running = false;
             }
+
+            on_key_type(&context, keycode);
 
             break;
 
@@ -74,7 +79,9 @@ int main(void) {
 }
 
 void render(struct Context *context) {
-    const struct Word current_word = context->game.words.pointer[0];
+    assert(context->game.words.length > 0, "game ended");
+    const struct Word current_word
+        = context->game.words.pointer[context->game.words.length - 1];
     struct CharVector current_char
         = string_at(current_word.pointer, context->game.char_index);
     const struct StringSlice *romas = get_roma(current_char.pointer);
@@ -108,9 +115,9 @@ void render(struct Context *context) {
 
     {
         for (size_t i = 0; i < current_word.char_count; i++) {
-            if (i < context->game.roma_index) {
+            if (i < context->game.char_index) {
                 term_set_fg(&TERMINAL, GREEN);
-            } else if (i == context->game.roma_index) {
+            } else if (i == context->game.char_index) {
                 term_set_fg(&TERMINAL, BLACK);
                 term_set_bg(&TERMINAL, RED);
             }
@@ -132,4 +139,41 @@ void render(struct Context *context) {
 
     term_set_cursor_pos(&TERMINAL, 0, 1);
     char_vector_free(&current_char);
+}
+
+void on_key_type(struct Context *context, char input) {
+    assert(context->game.words.length > 0, "game ended");
+    const struct Word current_word
+        = context->game.words.pointer[context->game.words.length - 1];
+    struct CharVector current_char
+        = string_at(current_word.pointer, context->game.char_index);
+    const struct StringSlice *romas = get_roma(current_char.pointer);
+
+    const size_t game_roma_index = context->game.roma_index;
+
+    for (size_t i = 0; i < romas->length; i++) {
+        const char *roma = romas->pointer[i];
+        const size_t roma_len = string_len(roma);
+
+        if (roma_len > game_roma_index && roma[game_roma_index] == input) {
+            // ³‚µ‚¢“ü—Í‚ª‚³‚ê‚½
+
+            if (roma_len - 1 == game_roma_index) {
+                // 1‚Ð‚ç‚ª‚È•¶Žš‚ÌÅŒã
+
+                context->game.roma_index = 0;
+                context->game.char_index += 1;
+
+                if (current_word.char_count == context->game.char_index) {
+                    // 1’PŒê‚ÌÅŒã
+                    word_vector_pop(&context->game.words);
+                    context->game.char_index = 0;
+                }
+
+                return;
+            }
+
+            context->game.roma_index += 1;
+        }
+    }
 }
