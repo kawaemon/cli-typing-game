7b098766c5c0fd4e54b6f67e9ffe470932fe252c
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-02-16 03:29:45 +0900

    fix: failure message now prints on stderr

--- src/assert.c
+++ src/assert.c
@@ -8,12 +8,13 @@ extern struct Terminal TERMINAL;
 void failure_hook(const char *filename, int line, const char *msg, ...) {
     term_reset(&TERMINAL);
 
-    printf("at %s line %d: ", filename, line);
+    fprintf(stderr, "at %s line %d: ", filename, line);
 
     va_list args;
     va_start(args, msg);
-    vprintf(msg, args);
+    vfprintf(stderr, msg, args);
     va_end(args);
 
+    fprintf(stderr, "\n");
     exit(1);
 }
