3e9e592370dae547029d23efc7e84f3c487abc80
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-02-16 04:42:55 +0900

    feat: result screen

--- src/main.c
+++ src/main.c
@@ -18,16 +18,29 @@ struct Game {
     struct WordVector words;
     size_t char_index;
     size_t roma_index;
+    bool previous_collect;
+};
+
+struct GameResult {
+    clock_t started_on;
+    clock_t ended_on;
+    uint64_t total_types;
+    double miss_types;
 };
 
 struct Context {
     bool running;
+    bool gaming;
     char last_key_code;
     struct Game game;
+    struct GameResult result;
 };
 
-void render(struct Context *context);
-void on_key_type(struct Context *context, char input);
+void game_render(struct Context *context);
+void game_on_key_type(struct Context *context, char input);
+
+void result_render(struct Context *context);
+void result_on_key_type(struct Context *context, char input);
 
 int main(void) {
     srand((unsigned)time(NULL));
@@ -41,18 +54,26 @@ int main(void) {
 
     struct Context context = {
         .running = true,
+        .gaming = true,
         .last_key_code = 0,
         .game = {
             .words = random_words(20),
             .char_index = 0,
             .roma_index = 0,
+            .previous_collect = true,
         },
+        .result = { 0 },
     };
 
     term_set_cursor_visible(&TERMINAL, false);
 
     do {
-        render(&context);
+        if (context.gaming) {
+            game_render(&context);
+        } else {
+            result_render(&context);
+        }
+
         const struct TerminalEvent event = term_poll_event(&TERMINAL);
 
         switch (event.type) {
@@ -63,7 +84,11 @@ int main(void) {
                 context.running = false;
             }
 
-            on_key_type(&context, keycode);
+            if (context.gaming) {
+                game_on_key_type(&context, keycode);
+            } else {
+                result_on_key_type(&context, keycode);
+            }
 
             break;
 
@@ -78,8 +103,14 @@ int main(void) {
     return 0;
 }
 
-void render(struct Context *context) {
-    assert(context->game.words.length > 0, "game ended");
+void game_render(struct Context *context) {
+    if (context->game.words.length == 0) {
+        // ゲーム終わり
+        context->gaming = false;
+        context->result.ended_on = clock();
+        return;
+    }
+
     const struct Word current_word
         = context->game.words.pointer[context->game.words.length - 1];
     struct CharVector current_char
@@ -89,7 +120,10 @@ void render(struct Context *context) {
     term_set_fg(&TERMINAL, WHITE);
     term_set_bg(&TERMINAL, BLACK);
 
-    term_print(&TERMINAL, "ESC: 終了 debug: %d\n", context->last_key_code);
+    term_print(&TERMINAL, "ESC: 終了 debug: %d %d %d %s\n",
+               context->last_key_code, context->result.total_types,
+               context->result.miss_types,
+               context->game.previous_collect ? "true" : "false");
     term_print(&TERMINAL, "NEXT: ");
 
     {
@@ -141,15 +175,20 @@ void render(struct Context *context) {
     char_vector_free(&current_char);
 }
 
-void on_key_type(struct Context *context, char input) {
+void game_on_key_type(struct Context *context, char input) {
     assert(context->game.words.length > 0, "game ended");
     const struct Word current_word
         = context->game.words.pointer[context->game.words.length - 1];
     struct CharVector current_char
         = string_at(current_word.pointer, context->game.char_index);
     const struct StringSlice *romas = get_roma(current_char.pointer);
-
     const size_t game_roma_index = context->game.roma_index;
+
+    if (context->result.started_on == 0) {
+        context->result.started_on = clock();
+    }
+
+    bool is_correct = false;
 
     for (size_t i = 0; i < romas->length; i++) {
         const char *roma = romas->pointer[i];
@@ -157,6 +196,8 @@ void on_key_type(struct Context *context, char input) {
 
         if (roma_len > game_roma_index && roma[game_roma_index] == input) {
             // 正しい入力がされた
+            context->result.total_types += 1;
+            is_correct = true;
 
             if (roma_len - 1 == game_roma_index) {
                 // 1ひらがな文字の最後
@@ -170,10 +211,36 @@ void on_key_type(struct Context *context, char input) {
                     context->game.char_index = 0;
                 }
 
-                return;
+                break;
             }
 
             context->game.roma_index += 1;
+            break;
         }
     }
+
+    if (!is_correct) {
+        context->result.miss_types += 1;
+    }
+
+    context->game.previous_collect = is_correct;
+}
+
+void result_render(struct Context *context) {
+    const struct GameResult result = context->result;
+    const double gameDurationSec = (double)(result.ended_on - result.started_on)
+                                   / (double)CLOCKS_PER_SEC;
+    const double keys_per_sec = result.total_types / gameDurationSec;
+
+    term_print(&TERMINAL, "終了: esc もう一度プレイ: space\n\n");
+
+    term_print(&TERMINAL, "ゲーム結果\n");
+    term_print(&TERMINAL, "かかった時間(秒): %02.02f\n", gameDurationSec);
+    term_print(&TERMINAL, "総キータイプ数: %d\n", result.total_types);
+    term_print(&TERMINAL, "ミスタイプ数: %d\n", result.miss_types);
+    term_print(&TERMINAL, "秒間キータイプ数: %02.02f\n", keys_per_sec);
+}
+
+void result_on_key_type(struct Context *context, char input) {
+    // todo
 }
